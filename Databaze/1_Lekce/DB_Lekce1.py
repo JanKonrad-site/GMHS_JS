88d31e74567ce2e36e3ee818a08d8c9484482c6b
pk <152068311+JanKonrad-site@users.noreply.github.com>
2026-03-04 11:06:53 +0100

    Update

--- Databaze/1_Lekce/DB_Lekce1.py
+++ Databaze/1_Lekce/DB_Lekce1.py
@@ -1,7 +1,9 @@
 import sqlite3
 from pathlib import Path
 
-cesta = Path(__file__).with_name("index.html")
+cesta = Path(__file__).with_name("database.db")
 print("Cesta:", cesta)
 print("Existuje databáze ve složce?", cesta.exists())
+
+
 
