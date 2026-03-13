76aaadd660f6a9ae5d04db574c82dbd6f2055a17
pk <152068311+JanKonrad-site@users.noreply.github.com>
2026-03-13 13:32:07 +0100

    Update

--- Databaze/2Lekce/DB_Lekce2.py
+++ Databaze/2Lekce/DB_Lekce2.py
@@ -1,30 +1,19 @@
 import sqlite3
 from pathlib import Path
 
-DB_FILE = Path(__file__).resolve().with_name("movies.db")
-def open_and_close_db():
-    print("DB path:", DB_FILE)
-    print("DB exists before connect:", DB_FILE.exists())
+DB_FILE = Path(__file__).with_name("movies.db")
 
-    conn = sqlite3.connect(str(DB_FILE))
-    conn.close()
-
-    print("DB exists after close:", DB_FILE.exists())
 def create_table_movies():
-    conn = sqlite3.connect(str(DB_FILE))
-
-    conn.execute("""
-    CREATE TABLE IF NOT EXISTS movies (
-        id    INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT    NOT NULL,
-        year  INTEGER
-    );
-    """)
-
+    conn = sqlite3.connect(DB_FILE)
+    conn.execute(""" CREATE TABLE IF NOT EXISTS movies (
+                 ID
+                 title
+                 year
+                 ); """)
     conn.commit()
     conn.close()
-if __name__ == "__main__":
-    open_and_close_db()
-    create_table_movies()
-    input("ENTER pro ukončení...")
 
+create_table_movies()
+
+
+   
