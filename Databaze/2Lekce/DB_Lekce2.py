8cc9c8911f79717284a95e3ba4c024f27db44c19
pk <152068311+JanKonrad-site@users.noreply.github.com>
2026-03-13 14:04:55 +0100

    uPDATE

--- Databaze/2Lekce/DB_Lekce2.py
+++ Databaze/2Lekce/DB_Lekce2.py
@@ -1,19 +1,22 @@
 import sqlite3
 from pathlib import Path
 
-DB_FILE = Path(__file__).with_name("movies.db")
+DB_FILE = Path(__file__).with_name("database.db")
 
 def create_table_movies():
     conn = sqlite3.connect(DB_FILE)
-    conn.execute(""" CREATE TABLE IF NOT EXISTS movies (
-                 ID
+
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS database(
+                 id
                  title
-                 year
-                 ); """)
+                 year); """)
     conn.commit()
     conn.close()
 
 create_table_movies()
+print("Cesta:", DB_FILE)
+print("Existuje databáze ve složce?", DB_FILE.exists())
 
 
-   
+
