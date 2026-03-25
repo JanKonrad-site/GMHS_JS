af32cd61076ad20053822e7379b956638a144c1f
pk <152068311+JanKonrad-site@users.noreply.github.com>
2026-03-25 10:59:36 +0100

    Update

--- Databaze/2Lekce/DB_Lekce2.py
+++ Databaze/2Lekce/DB_Lekce2.py
@@ -8,15 +8,24 @@ def create_table_movies():
 
     conn.execute("""
     CREATE TABLE IF NOT EXISTS database(
-                 id
-                 title
-                 year); """)
+                 id     INTEGER PRIMARY KEY AUTOINCREMENT,
+                 title  TEXT    NOT NULL,
+                 year   INTEGER
+                 ); """)
     conn.commit()
     conn.close()
+
+def print_table():
+    conn = sqlite3.connect(str(DB_FILE))
+    rows = conn.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+    conn.close()
+
+    print("Tabulky v databázi:", rows)
+    input("ENTER")
 
 create_table_movies()
 print("Cesta:", DB_FILE)
 print("Existuje databáze ve složce?", DB_FILE.exists())
-
+print_table()
 
 
